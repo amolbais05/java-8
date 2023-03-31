4d404bb3abaf1be3256cc0f08c1cecb10bb6587a
Amol Bais <amolbais05@gmail.com>
2023-03-31 23:44:24 +0530

    Practice problem

---
+++ src/main/java8/streams/practice/Employee.java
@@ -0,0 +1,23 @@
+package main.java8.streams.practice;
+
+public class Employee {
+
+    private String title;
+    private int salary;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
